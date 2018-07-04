6059607a7a3e0b933bba8c74a2ffa224217edec6
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-07-04 17:58:39 -0400

    working abstraction

--- Bad_Posture_Sensor/Bad_Posture_Sensor.ino
+++ Bad_Posture_Sensor/Bad_Posture_Sensor.ino
@@ -1,56 +1,64 @@
-int badPosturePin = 2; //button/pin to indicate if bad posture (when HIGH/pressed)
+int badPosturePin = 2; //button/pin to indicate if bad posture (when LOW/pressed)
 int beginnerLED = 10;  //beginner (slight) bad posture - lights up immediately
 int intermediateLED = 11;  //intermediate bad posture - lights up after 15 seconds
 int extremeLED = 12; //extremely bad posture - lights up after 30 seconds
-volatile bool badPosture = 0; //determined by state of badPosture pin
+volatile bool goodPosture = 0; //determined by state of badPosture pin
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);     //enable serial monitor
   DDRB |= (1 << 5);
   pinMode(badPosturePin, INPUT_PULLUP);  //but change to pull down if that's a thing
   pinMode(beginnerLED, OUTPUT);
   pinMode(intermediateLED, OUTPUT);
   pinMode(extremeLED, OUTPUT);
-  TCCR1A = 0;
+  TCCR1A = 0;     //reset  
   TCCR1B = 0;
-  TCCR1B = 3;
+  TCCR1B = 5;
 
   attachInterrupt(digitalPinToInterrupt(badPosturePin), postureHandler, CHANGE);  
 
-  TIMSK1 = 6; //enable OCR1A/B
-  OCR1A = 100; //100 seconds and LED flashes
-  OCR1B = 1000; //1000 seconds and LED flashes
+  OCR1A = 23437; //1.5 seconds and LED flashes
+  OCR1B = 46875; //3 seconds and LED flashes
 
 }
 
 void loop() {
-  if (badPosture) {
+  if (!goodPosture) {
    digitalWrite(beginnerLED, HIGH);
   }
   
-  if (!badPosture) {  //goodPosture
-   TCCR1B = 0; //pause time
+  if (goodPosture) {  //goodPosture
    digitalWrite(beginnerLED, LOW);
    digitalWrite(intermediateLED, LOW);
    digitalWrite(extremeLED, LOW);
-  
   }
 
 }
 
 void postureHandler(){
-  Serial.println("bad");
-  badPosture = digitalRead(badPosturePin);
-  TCNT1 = 0;  //start counting time to determine how long bad posture has been maintained
+  goodPosture = digitalRead(badPosturePin);
+  if(!goodPosture){
+      Serial.println("bad");
+      TCNT1 = 0;
+      TIMSK1 = 6; //enable OCR1A/B
+  }
+ if(goodPosture) {
+      Serial.println("good");
+      TIMSK1 = 0;   
+ }
+  
+  //start counting time to determine how long bad posture has been maintained
   return;
 }
 
 ISR (TIMER1_COMPA_vect) { //intermediate bad posture, after 100 seconds
+  Serial.println("1.5s");
   digitalWrite(intermediateLED, HIGH);
-  //insert delay?
+
 }
 
 ISR (TIMER1_COMPB_vect) { //intermediate bad posture, after 100 seconds
+  Serial.println("3s");
   digitalWrite(extremeLED, HIGH);
 }
 
