7ca90ff9e5d22baef1be15dd2abe5a39b6e7e7f8
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-07-04 17:27:04 -0400

    interrupt

--- Bad_Posture_Sensor/Bad_Posture_Sensor.ino
+++ Bad_Posture_Sensor/Bad_Posture_Sensor.ino
@@ -5,17 +5,17 @@ int extremeLED = 12; //extremely bad posture - lights up after 30 seconds
 volatile bool badPosture = 0; //determined by state of badPosture pin
 
 void setup() {
+  Serial.begin(9600);
   DDRB |= (1 << 5);
   pinMode(badPosturePin, INPUT_PULLUP);  //but change to pull down if that's a thing
   pinMode(beginnerLED, OUTPUT);
   pinMode(intermediateLED, OUTPUT);
   pinMode(extremeLED, OUTPUT);
-
   TCCR1A = 0;
   TCCR1B = 0;
   TCCR1B = 3;
 
-  attachInterrupt(digitalPinToInterrupt(badPosturePin), handler(), LOW);  //bad posture when button is high
+  attachInterrupt(digitalPinToInterrupt(badPosturePin), postureHandler, CHANGE);  
 
   TIMSK1 = 6; //enable OCR1A/B
   OCR1A = 100; //100 seconds and LED flashes
@@ -24,37 +24,34 @@ void setup() {
 }
 
 void loop() {
+  if (badPosture) {
+   digitalWrite(beginnerLED, HIGH);
+  }
   
-   if (badPosture) {
-    digitalWrite(beginnerLED, HIGH);
-   }
-  
-   else if (!badPosture) {  //if there is good Posture
-   TCCR1B = 0; //pause timer
+  if (!badPosture) {  //goodPosture
+   TCCR1B = 0; //pause time
    digitalWrite(beginnerLED, LOW);
    digitalWrite(intermediateLED, LOW);
    digitalWrite(extremeLED, LOW);
-   TCCR1B = 0x3;  //re-enable timer
+  
+  }
+
 }
 
-
-}
-
-void handler(){
+void postureHandler(){
+  Serial.println("bad");
   badPosture = 1;
   TCNT1 = 0;  //start counting time to determine how long bad posture has been maintained
-  //reset badPosture bool to 0?
+  return;
 }
 
 ISR (TIMER1_COMPA_vect) { //intermediate bad posture, after 100 seconds
   digitalWrite(intermediateLED, HIGH);
   //insert delay?
-  
 }
 
 ISR (TIMER1_COMPB_vect) { //intermediate bad posture, after 100 seconds
   digitalWrite(extremeLED, HIGH);
-  
 }
 
 
