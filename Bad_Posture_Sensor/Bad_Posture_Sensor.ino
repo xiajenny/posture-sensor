b1ade5527699b1bcebcb63eb26ee3c39c68d96b7
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-07-04 16:42:33 -0400

    pull-up

--- Bad_Posture_Sensor/Bad_Posture_Sensor.ino
+++ Bad_Posture_Sensor/Bad_Posture_Sensor.ino
@@ -1,12 +1,12 @@
 int badPosturePin = 2; //button/pin to indicate if bad posture (when HIGH/pressed)
 int beginnerLED = 10;  //beginner (slight) bad posture - lights up immediately
-int intermediateLED = 11;  //intermediate bad posture - lights up after 100 seconds
-int extremeLED = 12; //extremely bad posture - lights up after 1000 seconds
+int intermediateLED = 11;  //intermediate bad posture - lights up after 15 seconds
+int extremeLED = 12; //extremely bad posture - lights up after 30 seconds
 bool badPosture = 0; //determined by state of badPosture pin
 
 void setup() {
 DDRB |= (1 << 5);
-pinMode(badPosturePin, INPUT);  //but change to pull down if that's a thing
+pinMode(badPosturePin, INPUT_PULLUP);  //but change to pull down if that's a thing
 pinMode(beginnerLED, OUTPUT);
 pinMode(intermediateLED, OUTPUT);
 pinMode(extremeLED, OUTPUT);
@@ -15,7 +15,7 @@ TCCR1A = 0;
 TCCR1B = 0;
 TCCR1B = 3;
 
-attachInterrupt(digitalPinToInterrupt(badPosturePin), handler(), HIGH);  //bad posture when button is high
+attachInterrupt(digitalPinToInterrupt(badPosturePin), handler(), LOW);  //bad posture when button is high
 
 TIMSK1 = 6; //enable OCR1A/B
 OCR1A = 100; //100 seconds and LED flashes
@@ -24,8 +24,10 @@ OCR1B = 1000; //1000 seconds and LED flashes
 }
 
 void loop() {
-badPosture = digitalread(badPosturePin);  //read in handler?
-
+if (badPosture) {
+  digitalWrite(beginnerLED, HIGH);
+}
+  
 if (!badPosture) {  //goodPosture
   TCCR1B = 0; //pause time
   digitalWrite(beginnerLED, LOW);
@@ -38,7 +40,8 @@ if (!badPosture) {  //goodPosture
 }
 
 void handler(){
-  digitalWrite(beginnerLED, HIGH);  //light up first LED indicating change from good posture to bad posture
+  badPosture = 1;
+  //light up first LED indicating change from good posture to bad posture
   TCNT1 = 0;  //start counting time to determine how long bad posture has been maintained
   //reset badPosture bool to 0?
 }
