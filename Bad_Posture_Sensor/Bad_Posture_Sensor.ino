42d31184c6cf33cda490f438955edba3f39e8ba7
Sienna Wishart <sienna.wishart@gmail.com>
2018-07-04 16:34:35 -0400

    Added 1

--- Bad_Posture_Sensor/Bad_Posture_Sensor.ino
+++ Bad_Posture_Sensor/Bad_Posture_Sensor.ino
@@ -1,22 +1,59 @@
-int badPosture = 5; //pin to indicate if bad posture (when HIGH/pressed)
+int badPosturePin = 2; //button/pin to indicate if bad posture (when HIGH/pressed)
+int beginnerLED = 10;  //beginner (slight) bad posture - lights up immediately
+int intermediateLED = 11;  //intermediate bad posture - lights up after 100 seconds
+int extremeLED = 12; //extremely bad posture - lights up after 1000 seconds
+bool badPosture = 0; //determined by state of badPosture pin
 
 void setup() {
 DDRB |= (1 << 5);
-pinMode(badPosture, INPUT_PULLUP);
+pinMode(badPosturePin, INPUT);  //but change to pull down if that's a thing
+pinMode(beginnerLED, OUTPUT);
+pinMode(intermediateLED, OUTPUT);
+pinMode(extremeLED, OUTPUT);
 
 TCCR1A = 0;
 TCCR1B = 0;
 TCCR1B = 3;
 
-attachInterrupt(digitalPinToInterrupt(badPosture), handler(), HIGH);
+attachInterrupt(digitalPinToInterrupt(badPosturePin), handler(), HIGH);  //bad posture when button is high
 
-TIMSK1 = 6;
+TIMSK1 = 6; //enable OCR1A/B
 OCR1A = 100; //100 seconds and LED flashes
 OCR1B = 1000; //1000 seconds and LED flashes
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+badPosture = digitalread(badPosturePin);  //read in handler?
+
+if (!badPosture) {  //goodPosture
+  TCCR1B = 0; //pause time
+  digitalWrite(beginnerLED, LOW);
+  digitalWrite(intermediateLED, LOW);
+  digitalWrite(extremeLED, LOW);
+  
+}
+
 
 }
+
+void handler(){
+  digitalWrite(beginnerLED, HIGH);  //light up first LED indicating change from good posture to bad posture
+  TCNT1 = 0;  //start counting time to determine how long bad posture has been maintained
+  //reset badPosture bool to 0?
+}
+
+ISR (TIMER1_COMPA_vect) { //intermediate bad posture, after 100 seconds
+  digitalWrite(intermediateLED, HIGH);
+  //insert delay?
+  
+}
+
+ISR (TIMER1_COMPB_vect) { //intermediate bad posture, after 100 seconds
+  digitalWrite(extremeLED, HIGH);
+  
+}
+
+
+
+
