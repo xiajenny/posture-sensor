bb4210fafeed51e8d3506516bfd787bfbb149b95
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-07-05 19:01:26 -0400

    6-leds

--- Bad_Posture_Sensor/Bad_Posture_Sensor.ino
+++ Bad_Posture_Sensor/Bad_Posture_Sensor.ino
@@ -1,16 +1,37 @@
 int badPosturePin = 2; //button/pin to indicate if bad posture (when LOW/pressed)
+
 int beginnerLED = 10;  //beginner (slight) bad posture - lights up immediately
 int intermediateLED = 11;  //intermediate bad posture - lights up after 15 seconds
 int extremeLED = 12; //extremely bad posture - lights up after 30 seconds
+
+int backPin = 7;
+int shoulderPin = 8;
+int spinePin = 9;
+
+int backSensor = 4;
+int shoulderSensor = 5;
+int spineSensor = 6;
+
 volatile bool badPosture = 0; //determined by state of badPosture pin
 
 void setup() {
   Serial.begin(9600);     //enable serial monitor
-  DDRB |= (1 << 5);
+  DDRB |= (1 << 5);       //enable pin 13
+  
   pinMode(badPosturePin, INPUT_PULLUP);  //but change to pull down if that's a thing
+  
   pinMode(beginnerLED, OUTPUT);
   pinMode(intermediateLED, OUTPUT);
   pinMode(extremeLED, OUTPUT);
+
+  pinMode(backPin, OUTPUT);
+  pinMode(shoulderPin, OUTPUT);
+  pinMode(spinePin, OUTPUT);
+
+  pinMode(backSensor,INPUT);
+  pinMode(shoulderSensor, INPUT);
+  pinMode(spineSensor, INPUT);
+  
   TCCR1A = 0;     //reset  
   TCCR1B = 0;
   TCCR1B = 5;
@@ -25,12 +46,30 @@ void setup() {
 void loop() {
   if (badPosture) {
    digitalWrite(beginnerLED, HIGH);
+   if ( digitalRead(backSensor) ) 
+    digitalWrite(backPin, HIGH);
+   else 
+    digitalWrite(backPin, LOW);
+    
+   if ( digitalRead(shoulderSensor) ) 
+    digitalWrite(shoulderPin,HIGH);
+   else 
+    digitalWrite(shoulderPin, LOW);
+    
+   if ( digitalRead(spineSensor) ) 
+    digitalWrite(spinePin, HIGH);
+   else 
+    digitalWrite(spinePin, LOW);
   }
   
   if (!badPosture) {  //good Posture
    digitalWrite(beginnerLED, LOW);
    digitalWrite(intermediateLED, LOW);
    digitalWrite(extremeLED, LOW);
+
+   digitalWrite(backPin, LOW);
+   digitalWrite(shoulderPin, LOW);
+   digitalWrite(spinePin, LOW);
   }
 
 }
@@ -46,9 +85,6 @@ void postureHandler(){
       Serial.println("good");
       TIMSK1 = 0;   
  }
-  
-  //start counting time to determine how long bad posture has been maintained
-  return;
 }
 
 ISR (TIMER1_COMPA_vect) { //intermediate bad posture, after 100 seconds
